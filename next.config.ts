9bb1fcfc3d07c6f9d90233f146239a4f7acbe909
brunoricardocalaca-ctrl <brunoricardocalaca@gmail.com>
2026-01-20 02:57:54 -0300

    fix: move serverExternalPackages to top level in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["pdf-parse", "@thednp/dommatrix", "path2d-polyfill"]
 };
 
 export default nextConfig;
